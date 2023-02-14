a6a4d217330c61bebda62d595e7a52256641225f
Iulia <iulia@Iulias-MacBook-Pro.local>
2023-02-14 20:28:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('working');
+
+const toggleSwitch = document.querySelector(
+    '.theme-switch input[type="checkbox"]',
+);
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    if (currentTheme === 'light') {
+        document.querySelector('#darkmode').href = ''
+    }
+  if (currentTheme ==='dark')  {
+    toggleSwitch.checked = true;
+    document.querySelector('#darkmode').href = 'css/darkmode.css';
+  }
+}
+ function switchTheme(event) {
+ if (event.target.checked) {
+    document.querySelector('#darkmode').href = 'css/darkmode.css';
+    localStorage.setItem('theme', 'dark');
+ } else {
+    document.querySelector('#darkmode').href = '';
+    localStorage.setItem('theme', 'light');
+ }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
